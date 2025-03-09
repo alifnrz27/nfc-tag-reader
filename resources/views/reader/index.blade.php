0c3616ba1721585b1167e6d9ec29572319242aa1
alifnrz27 <alifnaufalrizki27@gmail.com>
2025-03-09 13:52:37 +0700

    edit

--- resources/views/reader/index.blade.php
+++ resources/views/reader/index.blade.php
@@ -43,7 +43,7 @@
             if ("NDEFReader" in window) {
                 const ndef = new NDEFReader();
                 try {
-                await ndef.write("Check out web.dev!");
+                await ndef.write("Check out web.dev! edit");
                 } catch(error) {
                 console.log(error);
                 }
